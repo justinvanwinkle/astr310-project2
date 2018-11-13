02c9af177f1e0b9e66d90ba17aad930d30d07285
Justin Van Winkle <justin.vanwinkle@gmail.com>
2018-11-13 15:31:14 -0500

    get tooth function working

--- fit_curves.py
+++ fit_curves.py
@@ -1,5 +1,21 @@
-from sklearn.metrics import mean_squared_error
+# from sklearn.metrics import mean_squared_error
 import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+def tooth_function(x, mag, center, max_time, transit_time):
+    y = np.zeros(x.shape)
+    start = center - max_time / 2 - transit_time
+    end = center + max_time / 2 + transit_time
+    y += np.logical_and(x >= start, x <= start + transit_time) * (
+        (mag / transit_time) * (x - start))
+    y += np.logical_and(
+        x >= start + transit_time, x <= start + transit_time + max_time) * (mag)
+    y += np.logical_and(x >= end - transit_time, x <= end) * (
+        (mag / transit_time) * (end - x))
+
+    return y
 
 
 def eclipsing_binary(dates, period, offset, usual, transit_time, dip1, dip2):
@@ -11,23 +27,27 @@ def flat_curve(dates, usual):
 
 
 def analyze_file(fn):
+    print(fn)
     days = []
     mags = []
     magerrs = []
 
     with open(fn) as f:
         for line in f:
-            day, mag, magerr = [float(x) for x in line.split()[:3]]
+            vals = line.split()
+            day = float(vals[0])
+            mag = float(vals[1])
+            magerr = float(vals[2])
             days.append(day)
-            mags.append(mags)
+            mags.append(mag)
             magerrs.append(magerr)
 
     days = np.array(days)
     mags = np.array(mags)
     magerrs = np.array(magerrs)
-
-
-
+    plt.errorbar(days, mags, yerr=magerrs, ecolor='red', fmt='o')
+    plt.show()
+    plt.clf()
 
 
 if __name__ == '__main__':
